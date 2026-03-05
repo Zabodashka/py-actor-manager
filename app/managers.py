b7df561b1181ea5c79cbc76222587e5f5239429e
Zabodashka <igorsulga6@gmail.com>
2026-03-05 12:38:51 +0200

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -1,43 +1,42 @@
+# app/managers.py
+
 import sqlite3
 from typing import List
-
 from app.models import Actor
 
 
 class ActorManager:
     def __init__(self, db_name: str, table_name: str) -> None:
-        self.db_name: str = db_name
-        self.table_name: str = table_name
-        self.conn: sqlite3.Connection = sqlite3.connect(self.db_name)
-        self.cursor: sqlite3.Cursor = self.conn.cursor()
-        self._create_table()
+        self.db_name = db_name
+        self.table_name = table_name
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        self._create_table_if_not_exists()
 
-    def _create_table(self) -> None:
+    def _create_table_if_not_exists(self) -> None:
         self.cursor.execute(
             f'CREATE TABLE IF NOT EXISTS {self.table_name} ('
-            'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+            'actor_id INTEGER PRIMARY KEY AUTOINCREMENT, '
             'first_name TEXT NOT NULL, '
-            'last_name TEXT NOT NULL'
-            ')'
+            'last_name TEXT NOT NULL)'
         )
         self.conn.commit()
 
     def create(self, first_name: str, last_name: str) -> None:
         self.cursor.execute(
-            f'INSERT INTO {self.table_name} '
-            '(first_name, last_name) VALUES (?, ?)',
+            f'INSERT INTO {self.table_name} (first_name, last_name) '
+            'VALUES (?, ?)',
             (first_name, last_name),
         )
         self.conn.commit()
 
     def all(self) -> List[Actor]:
         self.cursor.execute(
-            f'SELECT id, first_name, last_name FROM {self.table_name}'
+            f'SELECT actor_id, first_name, last_name FROM {self.table_name}'
         )
         rows = self.cursor.fetchall()
         return [
-            Actor(id=row[0], first_name=row[1], last_name=row[2])
-            for row in rows
+            Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows
         ]
 
     def update(
@@ -48,14 +47,14 @@ class ActorManager:
     ) -> None:
         self.cursor.execute(
             f'UPDATE {self.table_name} SET first_name = ?, last_name = ? '
-            'WHERE id = ?',
+            'WHERE actor_id = ?',
             (new_first_name, new_last_name, pk),
         )
         self.conn.commit()
 
     def delete(self, pk: int) -> None:
         self.cursor.execute(
-            f'DELETE FROM {self.table_name} WHERE id = ?',
+            f'DELETE FROM {self.table_name} WHERE actor_id = ?',
             (pk,),
         )
         self.conn.commit()
